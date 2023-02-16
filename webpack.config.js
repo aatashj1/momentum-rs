749b7e6aedafa9d79615c402671ca6b62028b0ab
aatashj1 <frnkxoxo69@gmail.com>
2023-02-16 22:34:39 +0300

    change settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
 
 module.exports = {
-  entry: './index.js',
+  entry: './scripts./index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'my-first-webpack.bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /\.[tj]s$/,
+      use: 'ts-loader',
+      exclude: /node_modules/,
+    }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
   },
 };
