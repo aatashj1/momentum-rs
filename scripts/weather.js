a0b919f3e18949a4139ef4e2742818c4e10de619
aatashj1 <frnkxoxo69@gmail.com>
2023-02-16 22:37:10 +0300

    add weather api

---
+++ scripts/weather.js
@@ -0,0 +1,23 @@
+
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const wind = document.querySelector('.wind');
+const weatherDescription = document.querySelector('.weather-description');
+
+const city = document.querySelector('.city');
+
+async function getWeather() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=83b4723393f25e2c076e5558fa794a27&units=metric`;
+  const res = await fetch(url);
+  const data = await res.json();
+
+  weatherIcon.className = 'weather-icon owf';
+  weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+  wind.textContent = `Wind speed: ${data.wind.speed}`
+  temperature.textContent = `${data.main.temp}°C`;
+  weatherDescription.textContent = data.weather[0].description;
+}
+
+city.addEventListener("change",getWeather);
+city.value = "Minsk";
+getWeather();
